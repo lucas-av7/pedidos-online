21182e57e6e5c157fac42adbd4b748e87bad85c7
Lucas Vasconcelos <lucas.av7@yahoo.com>
2020-07-17 07:06:41 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,42 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Header />
+    <router-view />
   </div>
 </template>
 
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  components: { Header }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  :root {
+    --main-color: #e63946;
+    --bg-color: #f1faee;
+    --headers-color: #1d3557;
+    --text-color: #457b9d;
+    --backup-color: #a8dadc;
+    font-size: 62.5%;
+  }
 
-#nav {
-  padding: 30px;
-}
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  body {
+    font-size: 1.6rem;
+    background-color: var(--bg-color);
+    color: var(--text-color);
+    font-family: 'Roboto', sans-serif;;
+  }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  h1, h2, h3, h4, h5, h6 {
+    color: var(--headers-color);
+  }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+  <header>
+    <div class="headerContent">
+      <h2>Pedido Online</h2>
+      <div class="menuMobile">
+        <div class="bar1"></div>
+        <div class="bar2"></div>
+        <div class="bar3"></div>
+      </div>
+    </div>
+    
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  header {
+    width: 100%;
+    height: 60px;
+    background-color: var(--main-color);
+  }
+
+  .headerContent {
+    width: 90%;
+    height: 100%;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .menuMobile {
+    width: 50px;
+    height: 50px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+  }
+
+  .menuMobile .bar1,
+  .menuMobile .bar2,
+  .menuMobile .bar3 {
+    width: 40px;
+    height: 5px;
+    background-color: var(--headers-color);
+  }
+</style>
