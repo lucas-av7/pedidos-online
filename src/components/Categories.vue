57ad0ec4886b224a6bee9188265e4cc59194ae7f
Lucas Vasconcelos <lucas.av7@yahoo.com>
2020-07-18 19:23:42 -0400

    Categoria de produtos dinâmica

---
+++ src/components/Categories.vue
@@ -0,0 +1,59 @@
+<template>
+  <section class="categories">
+    <div class="headerCategory">
+      <h1>{{ categoryName }}</h1>
+      <div>
+        <span class="dot"></span>
+        <span class="dot"></span>
+        <span class="dot"></span>
+      </div>
+    </div>
+    <div v-for="product in products" :key="product.id" class="products">
+      {{  product.name }}
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ['categoryName', 'products']
+}
+</script>
+
+<style>
+  .categories {
+    width: 90%;
+    margin: 20px auto 0;
+    border-radius: 7px;
+    background-color: var(--primary-color);
+    font-size: 1.5rem;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .headerCategory {
+    width: 100%;
+    height: 55px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px;
+    background-color: var(--main-color);
+    border-top-left-radius: 7px;
+    border-top-right-radius: 7px;
+  }
+
+  .dot {
+    height: 10px;
+    width: 10px;
+    background-color: var(--primary-color);
+    border-radius: 50%;
+    display: inline-block;
+    margin-left: 3px;
+  }
+
+  .products {
+    width: 100%;
+    padding: 20px;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,66 @@
   <div class="home">
     <StoreInfo />
     <Cart />
+    <Categories v-for="category in products" :key="category.id"
+      :categoryName="category.name" :products="category.products" />
   </div>
 </template>
 
 <script>
 import StoreInfo from '../components/StoreInfo.vue'
 import Cart from '../components/Cart.vue'
+import Categories from '../components/Categories.vue'
 
 export default {
-  components: { StoreInfo, Cart }
+  components: { StoreInfo, Cart, Categories },
+  data() {
+    return {
+      products: [
+        {
+        id: 1, 
+        name: 'Panquecas',
+        products: [
+          {
+            id: 1,
+            name: 'Panqueca de Frango com Molho Bechamel', 
+            description: 'Panqueca recheada com frango, queijo muçarela, molho bechamel, finalizada com queijo parmesão gratinado. Acompanhamento Opcional (Arroz Branco), sem custo.',
+            price: 17.00,
+            img: 'panqueca1.jpg'
+          },
+          {
+            id: 2,
+            name: 'Panqueca de Carne ao Molho Bechamel', 
+            description: 'Panqueca recheada com carne de boa qualidade, queijo mussarela, molho bechamel, finalizada com queijo parmesão gratinado. Acompanhamento Opcional (Arroz Branco), sem custo.',
+            price: 17.00,
+            img: 'panqueca2.jpg'
+          }
+        ]
+        },
+        {
+          id: 2,
+          name: 'Lasanhas',
+          products: [
+            {
+              id: 1,
+              name: 'Lasanha de Frango com Molho Bechamel', 
+              description: 'Lasanha de frango com molho bechamel, preparada com massa tradicional, recheada com muçarela, presunto e finalizando com o delicioso queijo parmesão gratinado.',
+              price: 31.90,
+              img: 'lasanha1.jpg'
+            },
+            {
+              id: 2,
+              name: 'Lasanha Tradicional de Carne com Molho Bechamel', 
+              description: 'Lasanha Tradicional, carne moída (alcatra ou contra filé), com molho bechamel, preparada com massa tradicional, recheada com muçarela, presunto, finalizando com o delicioso parmesão gratinado.',
+              price: 35.90,
+              img: 'lasanha2.jpg'
+            }
+          ]
+        }
+      ]
+    }
+  }
 }
+
 </script>
 
 <style>
