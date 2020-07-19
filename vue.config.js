cdd86fcfd40f5bf2b0919ac7c8068210ff2a4280
Lucas Vasconcelos <lucas.av7@yahoo.com>
2020-07-19 19:33:31 -0400

    Icones para o pwa

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,20 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/pedidos-online/'
-    : '/'
+    : '/',
+    pwa: {
+      name: 'Bora Cumê?',
+      themeColor: '#e63946',
+      msTileColor: '#fff',
+      appleMobileWebAppCapable: 'no',
+      appleMobileWebAppStatusBarStyle: 'default',
+  
+      // configure the workbox plugin
+      workboxPluginMode: 'InjectManifest',
+      workboxOptions: {
+        // swSrc is required in InjectManifest mode.
+        swSrc: 'dev/sw.js',
+        // ...other Workbox options...
+      }
+    }
 }
