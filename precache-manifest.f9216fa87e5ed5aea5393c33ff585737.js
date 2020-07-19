13295e1c7a3a901027cd148f34d13a29d8a28579
Lucas Vasconcelos <lucas.av7@yahoo.com>
2020-07-19 15:15:40 -0400

    Site online no GH-PAGE

---
+++ precache-manifest.f9216fa87e5ed5aea5393c33ff585737.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6342fb3ad8d4822488e3",
+    "url": "/pedidos-online/css/app.9f1bfcb2.css"
+  },
+  {
+    "revision": "9412536ea1058fe60a6433ca4a781fd0",
+    "url": "/pedidos-online/img/bg-repeat.9412536e.jpg"
+  },
+  {
+    "revision": "8a0c381eeac11591ddaf5b03c8270695",
+    "url": "/pedidos-online/img/feijoada1.8a0c381e.jpg"
+  },
+  {
+    "revision": "f9916e8c05c172cbc9d66b0dd2b52be2",
+    "url": "/pedidos-online/img/feijoada2.f9916e8c.jpg"
+  },
+  {
+    "revision": "a2b7db826984fb8177698f818b609705",
+    "url": "/pedidos-online/img/lasanha1.a2b7db82.jpg"
+  },
+  {
+    "revision": "91b11a4f9ba95e49e38eda2db015db8a",
+    "url": "/pedidos-online/img/lasanha2.91b11a4f.jpg"
+  },
+  {
+    "revision": "aa9f0071e7f2b043d584571e48322776",
+    "url": "/pedidos-online/img/logo.aa9f0071.png"
+  },
+  {
+    "revision": "5b8462402bf319c4fd9330a5b1d2b166",
+    "url": "/pedidos-online/img/panqueca1.5b846240.jpg"
+  },
+  {
+    "revision": "5d5ded3a67df207a3b05ad67a00d2374",
+    "url": "/pedidos-online/img/panqueca2.5d5ded3a.jpg"
+  },
+  {
+    "revision": "16756df0e37abbdaa5fb1d13e4e3c1f2",
+    "url": "/pedidos-online/img/whatsapp.16756df0.png"
+  },
+  {
+    "revision": "1b652c5e32667cb363fc6e561fb61747",
+    "url": "/pedidos-online/index.html"
+  },
+  {
+    "revision": "6342fb3ad8d4822488e3",
+    "url": "/pedidos-online/js/app.027bd8a0.js"
+  },
+  {
+    "revision": "1cad2359dac5fc63fcbb",
+    "url": "/pedidos-online/js/chunk-vendors.5b575f45.js"
+  },
+  {
+    "revision": "96be1f882960a34c084eabd170a6962b",
+    "url": "/pedidos-online/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pedidos-online/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/pedidos-online/precache-manifest.f9216fa87e5ed5aea5393c33ff585737.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "pedidos-online"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
