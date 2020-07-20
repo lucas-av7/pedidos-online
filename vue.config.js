2724f6d0b58fc38b63fac984f6a9ebb78535d899
Lucas Vasconcelos <lucas.av7@yahoo.com>
2020-07-19 20:13:01 -0400

    Editando o vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/pedidos-online/'
+    : '/',
+    pwa: {
+      name: 'Bora Cumê?',
+      themeColor: '#e63946',
+      msTileColor: '#fff',
+    }
+}
