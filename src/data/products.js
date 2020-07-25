dcec75430819ed6ffbb1077b61c58ccdc6f05823
Lucas Vasconcelos <lucas.av7@yahoo.com>
2020-07-25 19:36:50 -0400

    Separando produtos em arquivo dedicado

---
+++ src/data/products.js
@@ -0,0 +1,68 @@
+export default [
+  {
+  id: 1, 
+  name: 'Panquecas',
+  products: [
+    {
+      id: 1,
+      name: 'Panqueca de Frango com Molho Bechamel', 
+      description: 'Panqueca recheada com frango, queijo muçarela, molho bechamel, finalizada com queijo parmesão gratinado. Acompanhamento Opcional (Arroz Branco), sem custo.',
+      price: 17.00,
+      img: 'panqueca1.jpg',
+      amount: 0
+    },
+    {
+      id: 2,
+      name: 'Panqueca de Carne ao Molho Bechamel', 
+      description: 'Panqueca recheada com carne de boa qualidade, queijo mussarela, molho bechamel, finalizada com queijo parmesão gratinado. Acompanhamento Opcional (Arroz Branco), sem custo.',
+      price: 17.00,
+      img: 'panqueca2.jpg',
+      amount: 0
+    }
+  ]
+  },
+  {
+    id: 2,
+    name: 'Lasanhas',
+    products: [
+      {
+        id: 1,
+        name: 'Lasanha de Frango com Molho Bechamel', 
+        description: 'Lasanha de frango com molho bechamel, preparada com massa tradicional, recheada com muçarela, presunto e finalizando com o delicioso queijo parmesão gratinado.',
+        price: 31.90,
+        img: 'lasanha1.jpg',
+        amount: 0
+      },
+      {
+        id: 2,
+        name: 'Lasanha Tradicional de Carne com Molho Bechamel', 
+        description: 'Lasanha Tradicional, carne moída (alcatra ou contra filé), com molho bechamel, preparada com massa tradicional, recheada com muçarela, presunto, finalizando com o delicioso parmesão gratinado.',
+        price: 35.90,
+        img: 'lasanha2.jpg',
+        amount: 0
+      }
+    ]
+  },
+  {
+    id: 3,
+    name: 'Feijoadas',
+    products: [
+      {
+        id: 1,
+        name: 'Feijoada individual bora cumê', 
+        description: '750g incluindo os seguintes itens: feijoada, arroz, farofa, couve, laranja, e caldo quente apimentado.',
+        price: 25.00,
+        img: 'feijoada1.jpg',
+        amount: 0
+      },
+      {
+        id: 2,
+        name: 'Feijoada no pote bora cumê', 
+        description: '1 litro de feijoada no pote, acompanha arroz, farofa, couve, laranja, e caldo quente apimentado. serve 3 pessoas.',
+        price: 59.90,
+        img: 'feijoada2.jpg',
+        amount: 0
+      }
+    ]
+  }
+]
